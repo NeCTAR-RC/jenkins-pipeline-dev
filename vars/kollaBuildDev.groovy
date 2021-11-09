abca419296e8514f3692295910c33138e61f2712
Sam Morrison <sorrison@gmail.com>
2021-11-09 13:46:09 +1100

    dev

--- vars/kollaBuildDev.groovy
+++ vars/kollaBuildDev.groovy
@@ -1,16 +1,25 @@
-def call(String profile, String config_file) {
-    sh """#!/bin/bash -eu
+def call(String profile, String tag, String config_file = "etc/kolla-build.conf") {
+    sh """#!/bin/bash -eux
+    echo $tag
+    echo $profile
+    exit 1
     export REGISTRY_AUTH_FILE=auth.json
     echo "\$REGISTRY_PSW" | docker login -u "\$REGISTRY_USR" --password-stdin "\$REGISTRY_HOST"
     OPENSTACK_RELEASE=`echo \$GERRIT_BRANCH | awk -F '/' '{ print \$2 }'`
     virtualenv kolla
     . kolla/bin/activate
     pip install -r requirements.txt
-    #docker image prune --all -f
+    docker image prune --all -f
 
     docker pull \${REGISTRY_HOST}/kolla/ubuntu-source-base:\${OPENSTACK_RELEASE}
     docker pull \${REGISTRY_HOST}/kolla/ubuntu-source-openstack-base:\${OPENSTACK_RELEASE}
     kolla-build --config-file $config_file --profile $profile --skip-existing --tag \$OPENSTACK_RELEASE --registry \$REGISTRY_HOST
+
+    for image in `kolla-build --list-images --profile $profile --config-file $config_file | awk '{print \$3}' | egrep -v '^(base|openstack-base)\$'`
+    do
+      docker tag \${REGISTRY_HOST}/kolla/ubuntu-source-\${image}:\${OPENSTACK_RELEASE} \${REGISTRY_HOST}/kolla/ubuntu-source-\${image}:$tag
+      docker push \${REGISTRY_HOST}/kolla/ubuntu-source-\${image}:$tag
+    done
     """
 
 }
